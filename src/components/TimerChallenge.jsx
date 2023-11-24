c136dfcab5ea6e4ecb3916a80dc9d3b363485bf6
aminnicevic04 <aminnicevic@gmail.com>
2023-11-24 17:18:39 +0100

    update

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal.jsx";
 
+// let timer;
 function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  //ovde ne koristimo useState iz razloga jer useState okida rerenderovanje komponente i ako
+  //to komponenti nije potrebno a koristimo useref izl razloga jer ne okida rerenderovanje vec samo
+  //azurira vrednost refa
+  const dialog = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.showModal();
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <button>Start Challenge</button>
-      <p className="">Time is running...</p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>you lost</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+        <p className={timerStarted ? "active" : "undefined"}>
+          {timerStarted ? "Time is running..." : "Timer inactive "}
+        </p>
+      </section>
+    </>
   );
 }
 
